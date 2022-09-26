f6330c3f285d425a87986ecb7f706d0d559a8873
Andre Scherl <schoerlock@googlemail.com>
2022-09-26 13:24:02 +0200

    Erste Rennstrecke fertig

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,6 +2,34 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -25,7 +53,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level":
-            case "level1":return tiles.createTilemap(hex`1000100013131313131313131314141414131313131515130405050505050505050507131314141302010808090801010801021313141513020809040505050505050615131515130209090213141313131313151315131302080802131414141315151313040505060808030505050507131513150208010909080108090908021315131502090901090b0c0c0c0c0d021313131302080b0c0c0a0a0a0a0a0e02131413140208120a0a0a0a0a0a0a0e02131413140201120a0a0a0a0a0a0a0e0213141314020811101010101010100f02131513130305050505050505050505061515131513131515151313131315131314141315151314141514141315151513131313`, img`
+            case "level1":return tiles.createTilemap(hex`100010000a0a0c0a0c0b0b0c0b0a0a0a0c0c0c0a0a0a0a0a0f131313131313131313100a0a0c0c0a0d1514141414141414160e0a0a0b0b0a0d0e020203020101020d0e0a0a0b0c0a0d0e030f1313131313170e0c0a0c0c0a1c1d030d151414141414110c0a0c0a0a0d0e020d0e0b0b0b0a0c0c0a0c0f1313170e020d1813131313100c0a0c0d15141411021214141414160e0c0a0a0d0e0301030506060606070d0e0a0a0b0d0e0506060404040404080d0e0b0a0b0d0e0904040404040404080d0e0b0a0b0d0e1b191919191919191a0d0e0b0a0a0d18131313131313131313170e0c0a0c121414141414141414141414110c0a0c0c0a0b0b0c0b0b0a0c0c0c0a0a0a0a`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -42,7 +70,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn3,sprites.vehicle.roadTurn1,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn2,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath9,sprites.castle.tilePath8,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath7,myTiles.tile13,myTiles.tile14], TileScale.Sixteen);
         }
         return null;
     })
@@ -50,6 +78,34 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "RaceTrackVerticalLeft":
+            case "tile1":return tile1;
+            case "RaceTrackVerticalRight":
+            case "tile2":return tile2;
+            case "RaceTrackOuterTurnSouth":
+            case "tile4":return tile4;
+            case "RaceTrackOuterTurnEast":
+            case "tile3":return tile3;
+            case "RaceTrackOuterTurnWest":
+            case "tile5":return tile5;
+            case "RaceTrackOuterTurnNorth":
+            case "tile6":return tile6;
+            case "RaceTrackHorizontalUp":
+            case "tile7":return tile7;
+            case "RaceTrackHorizontalDown":
+            case "tile8":return tile8;
+            case "RaceTrackInnerTurnEast":
+            case "tile9":return tile9;
+            case "RaceTrackInnerTurnSouth":
+            case "tile10":return tile10;
+            case "RaceTrackInnerTurnWest":
+            case "tile11":return tile11;
+            case "RaceTrackInnerTurnNorth":
+            case "tile12":return tile12;
+            case "RaceTrackVerticalLeftStart":
+            case "tile13":return tile13;
+            case "RaceTrackVerticalRightStart":
+            case "tile14":return tile14;
         }
         return null;
     })
