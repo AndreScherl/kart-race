05783c419f55b0e1d09a83509ff163ed86720bf1
Andre Scherl <schoerlock@googlemail.com>
2022-09-28 17:19:52 +0200

    Startauto blau

--- images.g.ts
+++ images.g.ts
@@ -5,21 +5,21 @@ namespace myImages {
         switch(helpers.stringTrim(name)) {
             case "image1":
             case "car1 back":return img`
-. . . . . . e e c c e e . . . . 
-. . . . . e 2 2 2 2 2 2 e . . . 
-. . . . 2 c 2 2 2 2 2 2 c 2 . . 
-. . . e 2 c 4 2 2 2 2 2 c 2 e . 
-. . . f 2 2 4 2 2 2 2 2 c 2 f . 
-. . . f 2 2 4 2 2 2 2 2 2 2 f . 
-. . . f 2 2 4 2 2 2 2 2 2 2 f . 
-. . . f 2 c 2 4 4 2 2 2 c 2 f . 
-. . . e 2 c e c c c c e c 2 e . 
-. . . e 2 e c b b b b c e 2 e . 
-. . . e 2 e b b b b b b e 2 e . 
-. . . e e e e e e e e e e e e . 
-. . . f e d e e e e e e d e f . 
-. . . f e 2 d e e e e d 2 e f . 
-. . . f f e e e e e e e e f f . 
+. . . . . . 8 8 c c 8 8 . . . . 
+. . . . . 8 6 6 6 6 6 6 8 . . . 
+. . . . 6 c 6 6 6 6 6 6 c 6 . . 
+. . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
+. . . f 6 6 9 6 6 6 6 6 c 6 f . 
+. . . f 6 6 9 6 6 6 6 6 6 6 f . 
+. . . f 6 6 9 6 6 6 6 6 6 6 f . 
+. . . f 6 c 6 9 9 6 6 6 c 6 f . 
+. . . 8 6 c 8 c c c c 8 c 6 8 . 
+. . . 8 6 8 c b b b b c 8 6 8 . 
+. . . 8 6 8 b b b b b b 8 6 8 . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. . . f 8 d 8 8 8 8 8 8 d 8 f . 
+. . . f 8 6 d 8 8 8 8 d 6 8 f . 
+. . . f f 8 8 8 8 8 8 8 8 f f . 
 . . . . f f . . . . . . f f . . 
 `;
         }
